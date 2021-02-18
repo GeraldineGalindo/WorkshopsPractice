a5a66eefbc60a623112feb1c1e734b4b83da1b64
GeraldineGalindoG <widni.galindo@ucb.edu.bo>
2021-02-18 18:40:37 -0400

    [TSK-1] Created Workshop Model

---
+++ Workshops/Workshops/Models/Workshop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshops.Models
+{
+    public class Workshop
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Status status { get; set; }
+    }
+
+    public enum Status
+    {
+        SCHEDULED,
+        POSPONED,
+        CANCELLED
+    }
+
+}
