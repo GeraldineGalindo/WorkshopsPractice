e1d91838bdd02e22ab3019c9aa74cef2641eaf52
GeraldineGalindoG <widni.galindo@ucb.edu.bo>
2021-02-18 19:15:09 -0400

    [TSK-4] Added GET all workshops

---
+++ Workshops/Workshops/Controllers/WorkshopsController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Workshops.BusinessLogic;
+using Workshops.Models;
+
+namespace Workshops.Controllers
+{
+    
+    [ApiController]
+    public class WorkshopsController : Controller
+    {
+        private readonly IWorkshopService service;
+        public WorkshopsController(IWorkshopService service)
+        {
+            this.service = service;
+        }
+
+        [Route("api/workshops")]
+        [HttpGet]
+        public ActionResult<IEnumerable<Workshop>> GetAllWorkshops()
+        {
+            try
+            {
+                return Ok(service.GetAllWorkshops());
+            }
+            catch (Exception)
+            {
+
+                return NoContent();
+            }
+        }
+    }
+}
