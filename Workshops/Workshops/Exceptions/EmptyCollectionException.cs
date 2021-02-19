900ef8ec9aa32f4aeec8aba85501387e904b8732
GeraldineGalindoG <widni.galindo@ucb.edu.bo>
2021-02-18 21:22:32 -0400

    [TSK-12] Created basic exceptions

---
+++ Workshops/Workshops/Exceptions/EmptyCollectionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshops.Exceptions
+{
+    public class EmptyCollectionException : Exception
+    {
+        public EmptyCollectionException(string message) : base(message)
+        {
+                
+        }
+    }
+}
