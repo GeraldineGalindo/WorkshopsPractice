7afa13804c4c168f772d8b6da3760ebd5fdedce3
GeraldineGalindoG <widni.galindo@ucb.edu.bo>
2021-02-25 10:56:25 -0400

    [TSK-16] Deleted commented code on extensions

--- Workshops/WorkshopsLibrary/Extensions/ExceptionHandlingMiddlewareExtensions.cs
+++ Workshops/WorkshopsLibrary/Extensions/ExceptionHandlingMiddlewareExtensions.cs
@@ -12,40 +12,6 @@ namespace WorkshopsLibrary.Extensions
     {
         public static IApplicationBuilder UseNativeGlobalExceptionHandler(this IApplicationBuilder app)
         {
-            /*
-            app.UseExceptionHandler(errorApp =>
-            {
-                errorApp.Run(async context =>
-                {
-                    var errorFeature = context.Features.Get<IExceptionHandlerFeature>();
-                    var exception = errorFeature.Error;
-
-                    var errorResponse = new ErrorResponse();
-
-                    if (exception is DataMismatchException dataException)
-                    {
-                        errorResponse.StatusCode = StatusCodes.Status406NotAcceptable;
-                        errorResponse.Message = dataException.Message;
-                    }
-
-                    if (exception is EmptyCollectionException emptyException)
-                    {
-                        errorResponse.StatusCode = StatusCodes.Status204NoContent;
-                        errorResponse.Message = emptyException.Message;
-                    }
-
-                    if (exception is NotFoundItemException notFoundException)
-                    {
-                        errorResponse.StatusCode = StatusCodes.Status404NotFound;
-                        errorResponse.Message = notFoundException.Message;
-                    }
-
-                    context.Response.StatusCode = (int)errorResponse.StatusCode;
-                    context.Response.ContentType = "application/json";
-                    await context.Response.WriteAsync(errorResponse.ToJsonString());
-                });
-            });
-            */
             return app;
             
         }
